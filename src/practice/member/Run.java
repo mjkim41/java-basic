318aa6912b1ddc2e6f7772d6f5cee2022f1b158c
futuredeveloper41 <mj10283@naver.com>
2024-11-27 19:23:56 +0900

    11/27

---
+++ src/practice/member/Run.java
@@ -0,0 +1,8 @@
+package practice.member;
+
+public class Run {
+    public static void main(String[] args) {
+        MemberController mc = new MemberController();
+        mc.start();
+    }
+}
---
+++ src/practice/member/Test.java
@@ -0,0 +1,25 @@
+package practice.member;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        // 멤버 저장소 객체 생성
+        MemberRepository mr = new MemberRepository();
+
+        // 멤버 추가
+        mr.addMember(new Member("midol4@naver.com", "9999", "하몽덕후", Gender.THEY, 30));;
+
+        // 전체 회원 데이터 조회
+        Member[] members = mr.getMembers();
+        for (Member m : members) {
+            m.inform();
+        }
+
+        // 이메일 중복 검사
+        boolean flag = mr.isDuplicateEmail("abc123@def.com");
+        System.out.println("flag = " + flag);
+
+
+    }
+}
